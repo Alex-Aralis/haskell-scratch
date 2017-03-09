1a9d60935b87e4c29bb96a91fe6e42691ca50d4c
Alex Aralis <alex.aralis@gmail.com>
2017-03-08 22:52:10 -0500

    quicksort

--- scratch.hs
+++ scratch.hs
@@ -1,7 +1,13 @@
-zip' :: [a] -> [b] -> [(a,b)]
+qs :: Ord a => [a] -> [a]
 
-zip' [] ys = []
-zip' xs [] = []
-zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+qs [] = []
+qs (x:xs) = 
+  let split [] n = ([], [])
+      split (y:ys) n
+        | y <= n    = (y:lt, gt)
+        | otherwise = (lt, y:gt)
+        where (lt, gt) = split ys n
+      (lt, gt) = split xs x
+  in (qs lt) ++ [x] ++ (qs gt)
 
-main = (putStrLn . show) (zip' [1,2,3,4,5] ['a', 'b', 'c', 'd'])
+main = (putStrLn . show) (qs [3, 1, 5, 34, 2312, 1, 9])

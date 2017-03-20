4590a72556551115871348a6337b30c23512e77c
Alex Aralis <alex.aralis@gmail.com>
2017-03-20 17:02:59 -0400

    indulge the darkness that exists within all hearts

--- scratch.hs
+++ scratch.hs
@@ -1,5 +1,5 @@
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 
-zipWith' f xs ys = map (\(x, y) -> f x y) $ zip xs ys
+zipWith' = \f -> \xs -> map (\(x, y) -> f x y) . zip xs
 
 main = (putStrLn . show) (zipWith' (+) [1,2,3] [23,1,4])

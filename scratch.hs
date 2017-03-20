d8db0f72e2e3f4fddd398543a2ff7d20eb611f52
Alex Aralis <alex.aralis@gmail.com>
2017-03-20 15:54:36 -0400

    zipWith using application $ and lambda expressions

--- scratch.hs
+++ scratch.hs
@@ -1,6 +1,5 @@
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 
-zipWith' f xs ys = map (applyTuple f) (zip xs ys)
-  where applyTuple f (x, y) = f x y
+zipWith' f xs ys = map (\(x, y) -> f x y) $ zip xs ys
 
 main = (putStrLn . show) (zipWith' (+) [1,2,3] [23,1,4])

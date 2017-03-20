2845d218fd89c7bb17142e5a6aba7ae64c8b01f9
Alex Aralis <alex.aralis@gmail.com>
2017-03-20 14:13:20 -0400

    zipWith using recusion

--- scratch.hs
+++ scratch.hs
@@ -1,5 +1,7 @@
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 
-zipWith' f xs ys = [ f x y | (x, y) <- zip xs ys ]
+zipWith' f (x:xs) (y:ys) = (f x y):(zipWith' f xs ys)
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
 
 main = (putStrLn . show) (zipWith' (+) [1,2,3] [23,1,4])

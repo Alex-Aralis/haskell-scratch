f59c1a160470421860874c29d6730aac1649dfb9
Alex Aralis <alex.aralis@gmail.com>
2017-03-27 22:59:07 -0400

    make semifunctional tree printing statement

--- scratch.hs
+++ scratch.hs
@@ -1,34 +1,45 @@
 infixr 5 :-:
-data Tree a = a :-: (Maybe (Tree a), Maybe (Tree a)) deriving (Show, Read, Eq)
+data Tree a = EmptyTree 
+  | a :-: (Tree a, Tree a) 
+  deriving (Read, Eq)
 
-type MTree a = Maybe (Tree a)
+treePrint :: (Show a) => String -> Tree a -> String
+treePrint _ EmptyTree = ""
+treePrint pad (c:-:(l,r)) = line ++ (treePrint nPad l) ++ (treePrint nPad r) 
+  where 
+        cStr = "-" ++ (show c) ++ "-"
+        cLen = length cStr
+        nPad = pad ++ (replicate cLen ' ') ++ "|" 
+        line = pad ++ cStr ++ "|\n"
 
-leaf :: a -> MTree a
-leaf a = Just $ a:-:(Nothing,Nothing)
+instance (Show a) => Show (Tree a) where
+  show EmptyTree = "()"
+  show tree = treePrint "" tree
+
+leaf :: a -> Tree a
+leaf a = a:-:(EmptyTree,EmptyTree)
 
 infixr 5 -:-
-(-:-) :: (Ord a) => a -> MTree a -> MTree a
-(-:-) v Nothing = leaf v
-(-:-) v (Just node@(curr:-:(l,r))) = 
-  Just $ case v `compare` curr of
-    LT -> curr:-:(v-:-l,r)
-    GT -> curr:-:(l,v-:-r)
+(-:-) :: (Ord a) => a -> Tree a -> Tree a
+(-:-) v EmptyTree = leaf v
+(-:-) v node@(c:-:(l,r)) = 
+  case v `compare` c of
+    LT -> c:-:(v-:-l,r)
+    GT -> c:-:(l,v-:-r)
     EQ -> node
 
-treeElem :: (Ord a) => a -> MTree a -> Bool
-treeElem v Nothing = False
-treeElem v (Just (curr:-:(l,r))) =
-  case v `compare` curr of 
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem v EmptyTree = False
+treeElem v (c:-:(l,r)) =
+  case v `compare` c of 
     LT -> treeElem v l
     GT -> treeElem v r
     EQ -> True
 
--- flip :: (a -> b -> c) -> b -> a -> c
--- flip f y x = f x y
 
-fromList :: (Ord a) => [a] -> MTree a
-fromList xs = foldl (flip (-:-)) Nothing xs
+fromList :: (Ord a) => [a] -> Tree a
+fromList xs = foldl (flip (-:-)) EmptyTree xs
 
-tree = fromList [2,4,1,5,2]
+tree = fromList [2,4,1,5,2,-1 ,334,144,234,3, 9, 7,5,12,34]
 
 main = putStrLn . show $ tree 
